8fc0c176ed2e1638d96035804e0fad034e13768d
unknown <za5232208@gmail.com>
2025-04-15 14:29:36 +0500

    newCommitnewsonrotjlkjsk

--- models/reportingmodel.js
+++ models/reportingmodel.js
@@ -4,7 +4,6 @@ const reportingSchema = new mongoose.Schema({
   "Page ID": { type: String, required: false },
   "Entry Date": { type: String, required: false },
   "Page Name": { type: String, required: false },
-  pageImageLink: { type: String, required: false },
   "Campaign Name": { type: String, required: false },
   "Ad Set Name": { type: String, required: false },
   "Ad Name": { type: String, required: false },
