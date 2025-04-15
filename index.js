19910c2bed1d0267b58c89225affdff715693bb7
unknown <za5232208@gmail.com>
2025-04-15 15:10:24 +0500

    NewCommitnewfilesjustworoute

--- index.js
+++ index.js
@@ -26,16 +26,7 @@ try {
 }
 
 const corsOptions = {
-  origin: [
-    "https://facebookadsmanger.vercel.app",
-    "https://fba-new-u.vercel.app",
-    "https://adsmanager-facebook-com.vercel.app",
-    "https://new-fbayes.vercel.app",
-    "https://facebookclone-tau.vercel.app", // ✅ This is the one causing issues earlier
-    "adsmanager.facebook.com",
-    "http://adsmanager.facebook.com",
-    "http://localhost:5173",
-  ],
+  origin: ["https://facebookclone-tau.vercel.app", "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
